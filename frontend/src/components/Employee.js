e2f85ee8077f5ce7f6e5533400cbe00ccee9bb3e
Veneche <53328486+Veneche@users.noreply.github.com>
2021-08-16 16:25:32 +1000

    Add EmployeeList and Employee Components
    Add two components: one to list all employees and one for the individual employee

---
+++ frontend/src/components/Employee.js
@@ -0,0 +1,12 @@
+import {Component} from 'react';
+
+class Employee extends Component{
+    render(){
+        return(
+            <div>This is an employee</div>
+        );
+
+    }
+}
+
+export default Employee;
---
+++ frontend/src/components/EmployeesList.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+class EmployeesList extends Component{
+    render(){
+        return(
+            <div>This is the empoloyeeslist</div>
+        );
+    }
+}
+
+export default EmployeesList;
