cdd694e79695236ba78e8bd71d8bd1392748b7a4
Veneche <53328486+Veneche@users.noreply.github.com>
2021-09-30 13:11:27 +1000

    Added EmployeesList view and functionality

--- frontend/src/components/Employee.js
+++ frontend/src/components/Employee.js
@@ -1,9 +1,75 @@
 import {Component} from 'react';
+import "./Employee.css";
 
 class Employee extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isEditing: false,
+            newName: ""
+        };
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleUndo = this.handleUndo.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    componentDidMount(){
+        this.setState({
+            newName: this.props.name
+        });
+    }
+    handleEdit(){
+        //if editing client name, set state isEditing to true
+        this.setState({
+            isEditing: true
+        });
+    }
+    handleRemove(){
+        //handle remove client event
+        this.props.removeClient(this.props.id, this.props.name);
+    }
+    handleSave(){
+        //handle save of edited client name
+        this.props.editClient(this.props.id, this.state.newName);
+        //set state isEditing to false after saving
+        this.setState({
+            isEditing: false
+        });
+    }
+    handleUndo(){
+        //set state isEditing back to false
+        this.setState({
+            isEditing: false
+        });
+    }
+    handleNameChange(evt){
+        //onchange event on input - change new name to edited input value
+        this.setState({
+            newName: evt.target.value
+        });
+    }
+    
     render(){
         return(
-            <div>This is an employee: {this.props.name}</div>
+            <div className="Employee">
+                <div className="Employee-id">{this.props.id}</div>
+                <div className="Employee-name">
+                    {this.state.isEditing ? <input value={this.state.newName} onChange={this.handleNameChange}/> : this.props.name}
+                </div>
+
+                {this.state.isEditing ? (
+                    <div className="Employee-buttons">
+                        <div className="Employee-button" onClick={this.handleSave}><i class="fas fa-save"></i></div>
+                        <div className="Employee-button" onClick={this.handleUndo}><i class="fas fa-undo"></i></div>
+                    </div>
+                ) : (               
+                <div className="Employee-buttons">
+                    <div className="Employee-button" onClick={this.handleEdit}><i class="fas fa-edit"></i></div>
+                    <div className="Employee-button" onClick={this.handleRemove}><i class="fas fa-trash-alt"></i></div>
+                </div>
+                )}
+            </div>
         );
 
     }
--- frontend/src/components/EmployeesList.js
+++ frontend/src/components/EmployeesList.js
@@ -1,14 +1,27 @@
 import {Component} from 'react';
 import axios from 'axios';
 import Employee from './Employee';
+import "./EmployeesList.css";
 
 class EmployeesList extends Component{
     constructor(props){
         super(props);
         this.state = {
-            employees: []
-        }
+            employees: [],
+            filteredEmployees: [],
+            newEmployee: "",
+            searchKeyword: "",
+            isFiltered: false
+        };
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
+        this.handleNameInputChange = this.handleNameInputChange.bind(this);
+        this.handleFilterInputChange = this.handleFilterInputChange.bind(this);
+        this.clearInput = this.clearInput.bind(this);
+        this.removeClient = this.removeClient.bind(this);
+        this.editClient = this.editClient.bind(this);
     }
+    //get list of employees
     componentDidMount(){
         this.getEmployees();
     }
@@ -24,10 +37,134 @@ class EmployeesList extends Component{
             employees: employees
         });
     }
+    //Add new client to db and refresh client list
+    async handleAdd(){
+        let newEmployeeName = this.state.newEmployee;
+        if(newEmployeeName !== ""){
+            await axios.post("http://localhost:3000/employees",{
+                "name": this.state.newEmployee
+            })
+            .then((response) => {
+                console.log(response);
+                alert(`${newEmployeeName} added successfully`);
+            }, (error) => {
+                console.log(error);
+            });
+            this.getEmployees();
+            this.clearInput();
+        } else {
+            //if input is blank, display error to user
+            alert("Please enter a client name");
+        }
+        
+    }
+
+    handleFilter(){
+        let unfilteredEmployees = this.state.employees;
+        let filterKeyw = this.state.searchKeyword.toLowerCase();
+
+        console.log(filterKeyw);
+
+        if(filterKeyw.length>0){
+            let filteredEmployees = unfilteredEmployees.filter(client => (client.name.toLowerCase().indexOf(filterKeyw) > -1));
+
+            this.setState({
+                filteredEmployees: filteredEmployees
+            });
+            console.log(filteredEmployees);
+            this.setState({
+                isFiltered: true
+            });
+        } else {
+            this.setState({
+                isFiltered: false
+            });
+        }
+       
+    }
+
+    //set state to changed input value
+    handleNameInputChange(evt){
+        this.setState({
+            newEmployee: evt.target.value
+        });
+    }
+
+    handleFilterInputChange(evt){
+        this.setState({
+            searchKeyword: evt.target.value
+        });
+
+        
+    }
+
+    //clear newEmployee state and input
+    async clearInput(){
+        await this.setState({
+            newEmployee: ""
+        });
+    }
+
+    //delete selected client from db
+    //display success message
+    async removeClient(id, name){
+        await axios.delete(`http://localhost:3000/employees/${id}`)
+        .then((response) => {
+            console.log(response);
+            alert(`${name} removed successfully`);
+        }, (error) => {
+            console.log(error);
+        });
+        this.getEmployees();
+        
+    }
+
+    //update client name based on input value
+    async editClient(id, newName){
+        await axios.put(`http://localhost:3000/employees/${id}`,{name: newName})
+        .then((response) => {
+            console.log(response);
+            alert(`${newName} updated successfully`);
+        }, (error) => {
+            console.log(error);
+        });
+        this.getEmployees();
+    }
+
+
     render(){
         return(
-            <div>
-                {this.state.employees.map(emp => <Employee name={emp.name}/>)}
+            <div className="EmployeesList">
+                <div className="EmployeesList-add-filter">
+                    <div className="EmployeesList-filter">
+                        <label>
+                        <span className="EmployeesList-label">Filter By Employee Name:</span>
+                            <input type="text" onChange={this.handleFilterInputChange} placeholder="Enter Keyword" value={this.state.searchKeyword}/>
+                            <button onClick={this.handleFilter}>Filter</button>
+                            
+                        </label>
+                    </div>
+                    
+                    <div className="EmployeesList-add">
+                        <label>
+                            <span className="EmployeesList-label">Add New Employee:</span>
+                            <input type="text" onChange={this.handleNameInputChange} placeholder="Client Name" value={this.state.newEmployee}/>
+                            <button onClick={this.handleAdd}>Add</button>
+                        </label>
+                    </div>
+                    
+                      
+                </div>
+                <div className="EmployeesList-header">
+                    <div className="EmployeesList-id">Client ID</div>
+                    <div className="EmployeesList-name">Client Name</div>
+                </div>
+                {(this.state.isFiltered) ? (
+                    this.state.filteredEmployees.map(emp => <Employee editEmployee={this.editEmployee} removeEmployee={this.removeEmployee} id={emp.id} name={emp.name}/>)
+                ) : (
+                    this.state.employees.map(emp => <Employee editEmployee={this.editEmployee} removeEmployee={this.removeEmployee} id={emp.id} name={emp.name}/>)
+                )}
+                
             </div>
             
         );
